91c8efee2a7fb07116f1850c24e67f3b879a1fbb
kouaouci <karimakouaouci2803@gmail.com>
2021-12-02 09:57:24 +0100

    mise en place des exigences de la quete

---
+++ User.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import.java.util.List
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private Date birthdate;
+		private int size;
+		private  List <Photos> photo;
+		private  List   <Address>addresses; 
+	}
+
+}
